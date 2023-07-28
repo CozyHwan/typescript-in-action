5fb8a9947095768382f041ea50f14571f4bbd98f
jihwan <136334960+CozyHwan@users.noreply.github.com>
2023-07-28 10:21:19 +0000

    Typescript 컴파일러 구성을 설정하라

---
+++ app.js
@@ -0,0 +1,3 @@
+"use strict";
+console.log('Your codeing Style');
+//# sourceMappingURL=app.js.map
--- dist/main.c39d6dcf.js
+++ dist/main.c39d6dcf.js
@@ -119,6 +119,11 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   return newRequire;
 })({"main.ts":[function(require,module,exports) {
 "use strict";
+
+function repeatHello(count) {
+  return "hello".repeat(count);
+}
+console.log(repeatHello(1));
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -144,7 +149,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "37399" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44803" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
