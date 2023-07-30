492fd4f4a62853f397aaa5d2203b34e2485565d2
jihwan <136334960+CozyHwan@users.noreply.github.com>
2023-07-30 17:12:46 +0000

    객체 리터럴 기본 구문 내용을 정리하라
      * 객체 리터널은 식의 일종이다.
      * 프로퍼티는 프로티명: 식 형식의 구문을 사용해 정의 할 수 있다.

--- IntroductionToTypeScript/src/ch03.test.ts
+++ IntroductionToTypeScript/src/ch03.test.ts
@@ -15,3 +15,18 @@ describe('obj 객체', () => {
   })
 })
 
+/**
+ * 객체 리터널은 식의 일종이다.
+ * 프로퍼티는 프로퍼티명: 식 형식의 구문으로 정의한다.
+ */
+const name = input ? input : '이름 없음' // 조건 연산자를 사용 할 수 있다.
+const user = {
+  name: name,
+  age: 20, // 프로퍼티명: 식
+}
+
+describe('user 객체', () => {
+  test('프로퍼티 값이 올바르면 리턴을 한다.', () => {
+    expect(user.age).toBe(20)
+  })
+})
